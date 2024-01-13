3770435201ce5530ba26802205ec544e57f467d5
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2024-01-13 15:52:53 -0300

    feat: added route

--- lib/core/app/my_app.dart
+++ lib/core/app/my_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/core/routes/app_routes.dart';
+import 'package:flutter_clean_architecture/core/themes/my_theme.dart';
 import 'package:flutter_clean_architecture/features/user/presentation/screens/home_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,12 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Clean Arch',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+      theme: MyTheme.theme,
+      initialRoute: AppRoutes.home,
+      routes: {
+        AppRoutes.home: (_) => const HomeScreen(),
+      },
     );
   }
 }
